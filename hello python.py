5c47878e58cf08b1dbe58d0c890dc1eea7c14354
ian daniel <rosebelle@ubuntu.ubuntu-domain>
2015-03-04 16:42:16 +0300

    my first commit

---
+++ hello python.py
@@ -0,0 +1,6 @@
+Python 3.4.0 (default, Apr 11 2014, 13:05:11) 
+[GCC 4.8.2] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print ('hello world')
+hello world
+>>> 
---
+++ learning python.py
@@ -0,0 +1,8 @@
+Python 3.4.0 (default, Apr 11 2014, 13:05:11) 
+[GCC 4.8.2] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> age=25
+>>> name="ireen"
+>>> print ("{0} is {1} yeras old".format(name,age))
+ireen is 25 yeras old
+>>> 
